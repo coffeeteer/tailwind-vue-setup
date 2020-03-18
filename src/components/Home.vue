9fbfae69c16d4e517897641c701fbd5d882411b7
coffeeteer <tgrieshiemer@gmail.com>
2020-03-17 21:36:44 -0400

    Initial Commit
    With Starter files for Tailwind CSS and Vue js

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home flex justify-center flex-col items-center">
+      <h1 class="text-5xl">Home Page</h1>
+      <div class="flex flex-col items-center bg-gray-400 w-1/3 p-10 rounded-lg shadow-lg">
+          <img class="h-64 rounded-full" src="https://source.unsplash.com/random" alt="">
+          <h5 class="text-xl font-bold p-5">Some Image Text</h5>
+          <p class="text-left"><span class="text-teal-500 font-bold">Lorem ipsum</span> dolor, sit amet consectetur adipisicing elit. Enim ipsam veniam at sunt voluptate quam? Minima velit, consequuntur labore unde in officia repudiandae, hic amet quibusdam at dignissimos rem magnam.</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
